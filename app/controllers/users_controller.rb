ac005db18b40bc5ad0a2ac31a9a2da9ab50ab3e7
Zach Taylor <zachtaylorstudios@gmail.com>
2014-02-13 19:57:42 -0800

    Added functionality to edit users with authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -18,10 +22,36 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
--- spec/support/user_utilities.rb
+++ spec/support/user_utilities.rb
@@ -19,6 +19,20 @@ module UserUtilities
     user_with_same_email.save
   end
 
+  def sign_in(user, options={})
+    if options[:no_capybara]
+      # Sign in when not using Capybara.
+      remember_token = User.new_remember_token
+      cookies[:remember_token] = remember_token
+      user.update_attribute(:remember_token, User.encrypt(remember_token))
+    else
+      visit signin_path
+      fill_in "Email",    with: user.email
+      fill_in "Password", with: user.password
+      click_button "Sign in"
+    end
+  end
+
   RSpec::Matchers.define :not_have_valid_email do
     match do |user|
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
